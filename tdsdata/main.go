3b8e00abd1a1135d9150d705d34844f0157abcc9
Ayub Malik <ayub.malik@gmail.com>
2024-07-16 19:21:44 +0100

    wip

--- tdsdata/main.go
+++ tdsdata/main.go
@@ -1,111 +1,36 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/samber/lo"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
-
-type City struct {
-	CityId int    `json:"cityId"`
-	Name   string `json:"name"`
-}
-
-type Stop struct {
-	StopUuid    string `json:"stopUuid"`
-	StationName string `json:"stationName"`
-	StationCode string `json:"stationCode"`
-	City        City   `json:"city"`
-}
-
-type ODPair struct {
-	origin      Stop
-	destination Stop
-	key         string
-}
 
 func main() {
 	hc := http.Client{
 		Timeout: time.Second * 5,
 		Transport: &http.Transport{
-			MaxIdleConns:        10,
-			MaxIdleConnsPerHost: 10,
-			ReadBufferSize:      16 * 1024 * 1024,
+			MaxIdleConns:        8,
+			MaxIdleConnsPerHost: 8,
+			ReadBufferSize:      8 * 1024 * 1024,
 		},
 	}
 
 	apiKey := os.Getenv("API_KEY")
-	payload := `{"carrierId":221,"type":"ORIGIN"}`
-	stops, err := getStops(hc, apiKey, payload)
+
+	scraper := ScheduleScraper{
+		client:  hc,
+		apiKey:  apiKey,
+		baseUrl: "https://ride-api.bustickets.com/tickets",
+	}
+
+	schedules, err := scraper.Scrape()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return
 	}
 
-	fmt.Println("count:", len(stops))
-	var pairs []ODPair
-	for _, orig := range stops {
-		for _, dest := range stops {
-			var key string
-			if strings.Compare(orig.StationCode, dest.StationName) < 0 {
-				key = orig.StationCode + dest.StationCode
-			} else {
-				key = dest.StationCode + orig.StationCode
-			}
-
-			od := ODPair{
-				origin:      orig,
-				destination: dest,
-				key:         key,
-			}
-
-			pairs = append(pairs, od)
-		}
+	for _, s := range schedules {
+		fmt.Println(s)
 	}
-
-	pairs = lo.UniqBy(pairs, func(item ODPair) string {
-		return item.key
-	})
-
-	for _, pair := range pairs {
-		fmt.Println(pair.key)
-	}
-
-	fmt.Println("pairs2:", len(pairs))
-
-}
-
-func getStops(hc http.Client, apiKey, payload string) ([]Stop, error) {
-	req, err := http.NewRequest("POST", "https://ride-api.bustickets.com/tickets/stop", strings.NewReader(payload))
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %w", err)
-	}
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Tds-Api-Key", apiKey)
-	req.Header.Add("TDS-Carrier-Code", "PPB")
-	resp, err := hc.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error executing request: %w", err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("bad status code: %d, %s", resp.StatusCode, resp.Status)
-	}
-
-	var stops []Stop
-	decErr := json.NewDecoder(resp.Body).Decode(&stops)
-	err = resp.Body.Close()
-	if err != nil {
-		return nil, fmt.Errorf("error closing body: %w", err)
-	}
-
-	if decErr != nil {
-		return nil, fmt.Errorf("error decoding response: %w", decErr)
-	}
-	return stops, nil
 }
---
+++ tdsdata/tds_scraper.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/samber/lo"
+	"log"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+)
+
+type City struct {
+	CityId int    `json:"cityId"`
+	Name   string `json:"name"`
+}
+
+type Stop struct {
+	StopUuid    string `json:"stopUuid"`
+	StationName string `json:"stationName"`
+	StationCode string `json:"stationCode"`
+	City        City   `json:"city"`
+}
+
+type ODPair struct {
+	origin      Stop
+	destination Stop
+	key         string
+}
+
+type ScheduleScraper struct {
+	client  http.Client
+	apiKey  string
+	baseUrl string
+}
+
+func (s ScheduleScraper) Scrape() ([]ODCandidate, error) {
+	stops, err := s.getStops()
+	if err != nil {
+		return nil, fmt.Errorf("error getting stops: %w", err)
+	}
+
+	var pairs []ODPair
+	for _, orig := range stops {
+		for _, dest := range stops {
+			var key string
+			if strings.Compare(orig.StationCode, dest.StationCode) < 0 {
+				key = orig.StationCode + dest.StationCode
+			} else {
+				key = dest.StationCode + orig.StationCode
+			}
+
+			od := ODPair{
+				origin:      orig,
+				destination: dest,
+				key:         key,
+			}
+
+			pairs = append(pairs, od)
+		}
+	}
+
+	// filter so A->B === B->A
+	pairs = lo.UniqBy(pairs, func(item ODPair) string {
+		return item.key
+	})
+
+	pairs = lo.Shuffle(pairs)
+
+	odChan := make(chan ODPair)
+	go func() {
+		defer close(odChan)
+		for _, pair := range pairs {
+			odChan <- pair
+		}
+	}()
+
+	n := runtime.GOMAXPROCS(0)
+	wg := sync.WaitGroup{}
+	wg.Add(n)
+	start := time.Now()
+	for i := 0; i < n; i++ {
+		go func(id int) {
+			defer wg.Done()
+			for od := range odChan {
+				delay := 100 + rand.Intn(150)
+				time.Sleep(time.Duration(delay) * time.Millisecond)
+				log.Printf("routine %d, trying %s", id, od.key)
+				s.getSchedule(od)
+			}
+		}(i)
+	}
+	wg.Wait()
+	end := time.Now()
+	took := end.Sub(start)
+	log.Printf("took %s", took)
+	return nil, nil
+}
+
+func (s ScheduleScraper) getStops() ([]Stop, error) {
+	payload := `{"carrierId":221,"type":"ORIGIN"}`
+	url := s.baseUrl + "/stop"
+	req, err := http.NewRequest("POST", url, strings.NewReader(payload))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Tds-Api-Key", s.apiKey)
+	req.Header.Add("TDS-Carrier-Code", "PPB")
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error executing request: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad status code: %d, %s", resp.StatusCode, resp.Status)
+	}
+
+	var stops []Stop
+	decErr := json.NewDecoder(resp.Body).Decode(&stops)
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error closing body: %w", err)
+	}
+
+	if decErr != nil {
+		return nil, fmt.Errorf("error decoding response: %w", decErr)
+	}
+	return stops, nil
+}
+
+func (s ScheduleScraper) getSchedule(od ODPair) ([]ODCandidate, error) {
+	payload := `{
+			  "purchaseType": "SCHEDULE_BOOK",
+			  "origin": {
+				"stopUuid": "%s"
+			  },
+			  "destination": {
+				"stopUuid": "%d
+			  },
+			  "departDate": "2024-07-24,
+			  "cityMode": false,
+			  "isReturn": false,
+			  "passengerCounts": {
+				"Adult": 1
+			  }
+			}`
+	url := s.baseUrl + "/stop"
+	req, err := http.NewRequest("POST", url, strings.NewReader(payload))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Tds-Api-Key", s.apiKey)
+	req.Header.Add("TDS-Carrier-Code", "PPB")
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error executing request: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad status code: %d, %s", resp.StatusCode, resp.Status)
+	}
+
+	var stops []Stop
+	decErr := json.NewDecoder(resp.Body).Decode(&stops)
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error closing body: %w", err)
+	}
+
+	if decErr != nil {
+		return nil, fmt.Errorf("error decoding response: %w", decErr)
+	}
+	return stops, nil
+}
+
+type ODCandidate struct {
+	OD         ODPair
+	ScheduleID string
+}

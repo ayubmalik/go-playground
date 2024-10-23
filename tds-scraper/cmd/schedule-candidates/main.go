846b73d2c9fabba6fdd7054cfd693c285979127b
Ayub Malik <ayub.malik@gmail.com>
2024-10-23 18:03:41 +0100

    Tweak connection opts and shutdown signal

--- tds-scraper/cmd/schedule-candidates/main.go
+++ tds-scraper/cmd/schedule-candidates/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
-	"sync"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
@@ -17,7 +21,29 @@ func main() {
 		log.Fatalln("-creds flag needs to be set")
 	}
 
-	nc, err := nats.Connect("tls://connect.ngs.global", nats.UserCredentials(creds), nats.Name("pub.schedules.candidates"))
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	opts := []nats.Option{
+		nats.UserCredentials(creds),
+		nats.Name("sub.schedules.candidates"),
+		nats.ConnectHandler(func(c *nats.Conn) {
+			log.Println("Connected to", c.ConnectedUrl())
+		}),
+		nats.ReconnectWait(3 * time.Second),
+		nats.ReconnectHandler(func(c *nats.Conn) {
+			log.Println("Reconnected to", c.ConnectedUrl())
+		}),
+		nats.DisconnectHandler(func(c *nats.Conn) {
+			log.Println("Disconnected from NATS")
+		}),
+		nats.ClosedHandler(func(c *nats.Conn) {
+			log.Println("NATS connection is closed.")
+		}),
+		nats.NoReconnect(),
+	}
+
+	nc, err := nats.Connect("tls://connect.ngs.global", opts...)
 	if err != nil {
 		log.Fatalf("could not connect to nats %s\n", err)
 	}
@@ -26,16 +52,16 @@ func main() {
 		_ = nc.Drain()
 	}()
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
 	sub, err := nc.QueueSubscribe("tds.schedules.candidates", "candidates", func(msg *nats.Msg) {
 		log.Printf("- %s - got msg: %s", msg.Header.Get(nats.MsgIdHdr), string(msg.Data))
 	})
 	if err != nil {
-		wg.Done()
+		//		stop()
 		log.Fatal(err)
 	}
-	wg.Wait()
+
+	<-ctx.Done()
+	log.Println("draining sub and conn")
 	_ = sub.Drain()
 
 }

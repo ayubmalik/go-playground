1e1a9b0a46169d5cd80b3125303e940e25031576
Ayub Malik <ayub.malik@gmail.com>
2024-10-28 20:31:57 +0000

    Tests

--- tds-scraper/cmd/schedule-candidates/main_test.go
+++ tds-scraper/cmd/schedule-candidates/main_test.go
@@ -3,19 +3,32 @@ package main
 import "testing"
 
 type stubClient struct {
-	count int
+	count       int
+	origin      string
+	destination string
 }
 
-func (s stubClient) FindSchedules(qry ScheduleQuery) (ScheduleResult, error) {
+func (s *stubClient) FindSchedules(qry ScheduleQuery) (ScheduleResult, error) {
 	s.count++
+	s.origin = qry.Origin.StopUuid
+	s.destination = qry.Destination.StopUuid
+	return ScheduleResult{}, nil
 }
 
 func TestTrySchedule(t *testing.T) {
-
-	tds := stubClient{}
+	tds := &stubClient{}
 	t.Run("days range", func(t *testing.T) {
-		if err := trySchedule(tds, 2, "origin", "dest"); err != nil {
-			t.Errorf("not trySchedule error %v", err)
+		_ = trySchedule(tds, 7, "origin", "dest")
+		if tds.count != 7 {
+			t.Errorf("client calls = %d, want 2", tds.count)
+		}
+
+		if tds.origin != "origin" {
+			t.Errorf("origin = %s, want origin", tds.origin)
+		}
+
+		if tds.destination != "dest" {
+			t.Errorf("destination = %s, want dest", tds.destination)
 		}
 	})
 }

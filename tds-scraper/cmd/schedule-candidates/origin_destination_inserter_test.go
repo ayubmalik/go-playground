8dec56309d02c91e5bcb0e3e740284d406aa84bf
Ayub Malik <ayub.malik@gmail.com>
2024-11-15 19:00:08 +0100

    WIP

--- tds-scraper/cmd/schedule-candidates/origin_destination_inserter_test.go
+++ tds-scraper/cmd/schedule-candidates/origin_destination_inserter_test.go
@@ -1,9 +1,16 @@
 package main_test
 
 import (
+	"encoding/json"
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 	"testing"
 )
+
+type Stop struct {
+	StopUuid string `json:"stopUuid"`
+	City     string
+}
 
 func TestKVSpike(t *testing.T) {
 	credentials := "/home/ayub/Downloads/NGS-Default-user_pub_ayub.creds"
@@ -27,13 +34,33 @@ func TestKVSpike(t *testing.T) {
 
 	t.Run("put", func(t *testing.T) {
 		kv, err := js.CreateKeyValue(&nats.KeyValueConfig{
-			Bucket: "originDestinations",
+			Bucket: "OriginDestinationsX",
 		})
+
 		if err != nil {
 			t.Fatalf("could not create kv %s\n", err)
 		}
 
 		_, err = kv.Put("x", []byte("hello"))
+
+		dests := make([]Stop, 0, 100)
+		for range 5 {
+			id := uuid.New().String()
+			stop := Stop{StopUuid: id, City: "New York"}
+			key, _ := json.Marshal(stop)
+			entry, err := kv.Get(string(key))
+			if err != nil {
+				t.Fatalf("could not get kv %s\n", err)
+			}
+
+			err = json.Unmarshal(entry.Value(), &dests)
+			if err != nil {
+				t.Fatalf("could not unmarshal kv %s\n", err)
+			}
+
+			t.Logf("dests: %v\n", dests)
+		}
+
 		if err != nil {
 			t.Fatalf("could not put value: %s\n", err)
 		}
